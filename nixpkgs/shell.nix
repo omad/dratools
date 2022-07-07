26497d2c0834e3060c6c5219f611cab3ccddff65
Damien Ayers <damien@omad.net>
2022-07-07 11:09:30 +1000

    Theengs Gateway - Better Nix

---
+++ nixpkgs/shell.nix
@@ -0,0 +1,8 @@
+with import <nixpkgs> {};
+
+( let
+    TheengsGateway = callPackage ./theengs-gateway.nix {
+      buildPythonPackage = python39Packages.buildPythonPackage;
+    };
+  in python39.withPackages (ps: [ TheengsGateway ])
+).env
--- nixpkgs/theengs-gateway.nix
+++ nixpkgs/theengs-gateway.nix
@@ -1,39 +1,32 @@
-{ pkgs ? import <nixpkgs> {}
-}:
+{ lib, buildPythonPackage, cmake, ninja, python39Packages } :
 
-with pkgs; 
-python3Packages.buildPythonPackage rec {
+buildPythonPackage rec {
   pname = "TheengsGateway";
   version = "0.2.0";
   format = "pyproject";
 
-  src = python3Packages.fetchPypi {
+  src = python39Packages.fetchPypi {
     inherit pname version;
     sha256 = "sha256-TjQzlXweLL5N0effNUpayxRhVlKoFDw1R5RIb2vuVpI=";
   };
-  preBuild = ''
-    echo ####### PRE BUILD DAMIEN ######
-    ls -l
-    ls -l TheengsDecoder
-    ls -l TheengsDecoder/python/
-    ls -l TheengsDecoder/python/TheengsDecoder/
-  '';
-#  src = fetchFromGitHub {
-#    owner = "theengs";
-#    repo = "gateway";
-#    rev = "v${version}";
-#    sha256 = "sha256-mbNZ21hclVqPPqJG6B1HvI8M5Sq5zMVVSx4T6OJaHds=";
-#    fetchSubmodules = true;
-#  };
+
   nativeBuildInputs = [ 
     cmake 
     ninja
-    python3Packages.scikit-build];
+    python39Packages.scikit-build
+  ];
 
   # setup.py will always (re-)execute cmake in buildPhase
   dontConfigure = true;
-  propagatedBuildInputs = [ 
-    python3Packages.paho-mqtt
-    python3Packages.bleak
+
+  propagatedBuildInputs = with python39Packages; [
+    paho-mqtt
+    bleak
   ];
+
+  meta = with lib; {
+    homepage = "https://github.com/theengs/gateway";
+    description = "BLE to MQTT Gateway";
+    license = licenses.gpl3;
+  };
 }
